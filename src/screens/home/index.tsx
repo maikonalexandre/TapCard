3432ebc752f467cfe4d305799471b09d190f1ce2
Maike <maikonalexandre574@gmail.com>
2023-12-29 18:27:48 -0300

    feat: create home page

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,32 @@
-import { View, Text } from "react-native";
 import { Header } from "../../components/Header";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Container } from "../../components/Container";
+import React from "react";
+import { TouchableOpacity, View, Text, ScrollView } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+import colors from "tailwindcss/colors";
+import { Deck } from "../../components/Deck";
 
 export function Home() {
   return (
-    <SafeAreaView className="bg-red-500 p-4">
-      <View>
-        <Header />
+    <Container>
+      <Header />
+      {/* <Text className="pt-4 text-base font-medium text-neutral-700 mb-2">
+        Decks:
+      </Text> */}
+      <Deck />
+
+      <View className="flex-1 items-center justify-center px-8">
+        <Text className="text-center text-base font-medium text-neutral-400">
+          Aqui estão todos os seus baralhos para revisão...
+        </Text>
       </View>
-    </SafeAreaView>
+      <TouchableOpacity
+        activeOpacity={0.2}
+        className="absolute bottom-8 right-8 bg-orange-500 p-4 rounded-full"
+      >
+        <Feather name="plus" size={20} color={colors.stone[100]} />
+      </TouchableOpacity>
+    </Container>
   );
 }
