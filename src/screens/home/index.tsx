9a1413fd1f4756617ab5436c0b0c149e630a71a7
Maike <maikonalexandre574@gmail.com>
2023-12-29 16:50:11 -0300

    feat: create screen home

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
+import { Header } from "../../components/Header";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export function Home() {
   return (
-    <View className="bg-stone-100">
-      <Text>Hello</Text>
-    </View>
+    <SafeAreaView className="bg-red-500 p-4">
+      <View>
+        <Header />
+      </View>
+    </SafeAreaView>
   );
 }
