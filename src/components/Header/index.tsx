93074086aa1891c7ccde549176e7da155a6da7d1
Maike <maikonalexandre574@gmail.com>
2023-12-29 18:27:29 -0300

    create: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Feather } from "@expo/vector-icons";
 
 export function Header() {
   return (
-    <View className="flex-row items-center justify-between">
+    <View className="flex-row items-center justify-between pb-2 border-b border-b-neutral-300">
       <Text className="font-bold text-lg">TapCards</Text>
       <TouchableOpacity activeOpacity={0.2} className="rounded-full p-1">
         <Feather name="settings" size={20} />
