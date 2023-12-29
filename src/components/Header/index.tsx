7af9ac6e9761dc2d5c76290506c20b9faa39b7fc
Maike <maikonalexandre574@gmail.com>
2023-12-29 17:27:30 -0300

    feat: add component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+export function Header() {
+  return (
+    <View className="flex-row items-center justify-between">
+      <Text className="font-bold text-lg">TapCards</Text>
+      <TouchableOpacity activeOpacity={0.2} className="rounded-full p-1">
+        <Feather name="settings" size={20} />
+      </TouchableOpacity>
+    </View>
+  );
+}
