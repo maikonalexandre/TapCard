57aa1390b7b5c2f4bfce59fd714dfa76b4d65ec1
Maike <maikonalexandre574@gmail.com>
2023-12-29 18:27:09 -0300

    feat: add deck card

---
+++ src/components/Deck/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const Deck = () => {
+  return (
+    <View className="w-full px-6 py-4 rounded-2xl mt-4 border border-zinc-200 flex flex-row justify-between items-center">
+      <Text className="text-base font-medium text-zinc-600">Ingles</Text>
+    </View>
+  );
+};
