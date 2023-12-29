8c0cf1816fc21df7bf9ab47d69e0b0a23ef44ffa
Maike <maikonalexandre574@gmail.com>
2023-12-29 16:49:50 -0300

    style: remove view

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Routes } from "./src/routes";
+import React from "react";
 
 export default function App() {
   return (
-    <View className="h-full w-full p-6 antialiased bg-stone-100">
+    <>
       <Routes />
       <StatusBar style="dark" />
-    </View>
+    </>
   );
 }
