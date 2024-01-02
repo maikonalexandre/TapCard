d1e57b629816a8440a004686d9ca4c418160dde0
Maike <maikonalexandre574@gmail.com>
2024-01-02 14:39:04 -0300

    refactor: add circle bar progress

--- src/components/Deck/index.tsx
+++ src/components/Deck/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { CircularProgress } from "react-native-circular-progress";
+import colors from "tailwindcss/colors";
 
-export const Deck = () => {
+interface DeckProps {
+  title: string;
+  percentage?: number;
+}
+
+export const Deck = ({ title, percentage = 0 }: DeckProps) => {
   return (
-    <View className="w-full px-6 py-4 rounded-2xl mt-4 border border-zinc-200 flex flex-row justify-between items-center">
-      <Text className="text-base font-medium text-zinc-600">Ingles</Text>
+    <View className="w-full px-6 py-4 rounded-2xl mt-4 border border-zinc-200 ">
+      <TouchableOpacity
+        activeOpacity={0.2}
+        className="flex flex-row justify-between items-center"
+      >
+        <Text className="text-base font-medium text-zinc-600">{title}</Text>
+        <CircularProgress
+          size={30}
+          width={2}
+          fill={percentage}
+          tintColor={colors.emerald[500]}
+          backgroundColor={colors.stone[300]}
+        >
+          {(fill) => (
+            <Text className="text-[10px]  text-emerald-600">{fill}%</Text>
+          )}
+        </CircularProgress>
+      </TouchableOpacity>
     </View>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
 
 export function Header() {
   return (
     <View className="flex-row items-center justify-between pb-2 border-b border-b-neutral-300">
-      <Text className="font-bold text-lg">TapCards</Text>
+      <Text className="font-bold text-lg text-neutral-900">TapCards</Text>
       <TouchableOpacity activeOpacity={0.2} className="rounded-full p-1">
-        <Feather name="settings" size={20} />
+        <Feather name="settings" size={20} color={colors.neutral[500]} />
       </TouchableOpacity>
     </View>
   );
